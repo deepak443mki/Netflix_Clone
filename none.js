2699dde4ea5b3cf14c22d53e72f16e2509658524
Deepak Kumar Majhi <mdeepakkumar443@gmail.com>
2024-07-31 10:48:31 +0530

    first commit

---
+++ none.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (validateEmail(email) && validatePassword(password)) {
+        alert('Successfully logged in');
+        window.location.href = "browse.html";
+    } else {
+        alert('Invalid email or password');
+    }
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 6;
+}
